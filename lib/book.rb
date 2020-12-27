0df612b32a087f33f3be7d99e46c78f2f3de0354
Jacqueline Cournoyer <jcournoyer5@gmail.com>
2020-12-27 13:18:48 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,18 @@
+`
 
+class Book
 
+  def book=(title)
+    @title = book
+  end
+
+  def name
+    @title
+  end
+
+And Then There Were None = Book.new
+And Then There Were None.title = "And Then There Were None"
+
+puts And Then There Were None.title
+
+ end
