3d6a412732605394fc2738d71489734301140e32
Jacqueline Cournoyer <jcournoyer5@gmail.com>
2021-01-03 16:30:02 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -7,8 +7,7 @@ class Shoe
   end
 
   def cobble
-    puts "Your shoe is as good as new!"
-    shoe.condition == "new"
+    puts "Your shoe is as good as new!", Shoe.condition == "new"
   end
 
 end 
