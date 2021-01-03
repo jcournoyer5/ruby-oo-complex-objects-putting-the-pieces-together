c575bdcb43b1a9e28399c4d45c45cb8143e4d77a
Jacqueline Cournoyer <jcournoyer5@gmail.com>
2021-01-03 16:22:15 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,17 +1,44 @@
-
-
 class Book
-
-  def book=(title)
-    @title = book
+  def initialize(title)
+    @title = title
   end
 
-  def name
+  def title
     @title
   end
-  
-end
-# And Then There Were None = Book.new
-# And Then There Were None.title = "And Then There Were None"
 
-# And Then There Were None.title
+  def author=(author)
+    @author = author
+  end
+
+  def author
+    @author
+  end
+
+  def page_count=(num)
+    @page_count = num
+  end
+
+  def page_count
+    @page_count
+  end
+
+  def genre=(genre)
+    @genre = genre
+  end
+
+  def genre
+    @genre
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+
+end 
+  #end  
+
+#And Then There Were None = Book.new
+#And Then There Were None.title = "And Then There Were None"
+
+#And Then There Were None.title
